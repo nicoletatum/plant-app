7d6b6866521b480d030de248376c4dd64cb86f1b
nicoletatum <nicoletatum@outlook.com>
2021-06-14 22:08:04 -0500

    login working

--- plantapp/settings.py
+++ plantapp/settings.py
@@ -59,7 +59,6 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 )
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
