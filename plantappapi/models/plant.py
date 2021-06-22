08100a512cde20056ba9c7e4ffa4b6d36a4b731d
nicoletatum <nicoletatum@outlook.com>
2021-06-22 14:23:14 -0500

    finally at mvp

--- plantappapi/models/plant.py
+++ plantappapi/models/plant.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-
 class Plant(models.Model):
-    
     name = models.CharField(max_length=50)
     light_level = models.ForeignKey("Light", on_delete=models.CASCADE)
     water_amount = models.ForeignKey("Water", on_delete=models.CASCADE)
@@ -12,3 +10,4 @@ class Plant(models.Model):
     temp_needs = models.CharField(max_length=200)
     notes = models.CharField(max_length=200)
     plant_pic = models.URLField(null=True)
+    pest_watch = models.BooleanField(default=False)
--- seed_data.sh
+++ seed_data.sh
@@ -1,14 +1,14 @@
-rm -rf plantappapi/migrations
+# rm -rf plantappapi/migrations
 
-rm db.sqlite3
+# rm db.sqlite3
 
-python manage.py makemigrations plantappapi
+# python manage.py makemigrations plantappapi
 
-python manage.py migrate 
+# python manage.py migrate 
 
-python manage.py loaddata users
-python manage.py loaddata plantowners
-python manage.py loaddata tokens
-python manage.py loaddata lights
-python manage.py loaddata water
-python manage.py loaddata plant
+# python manage.py loaddata users
+# python manage.py loaddata plantowners
+# python manage.py loaddata tokens
+# python manage.py loaddata lights
+# python manage.py loaddata water
+# python manage.py loaddata plant
