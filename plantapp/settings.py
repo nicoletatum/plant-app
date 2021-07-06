7fb83c9e80f8d68505d5857156e669be313028c0
nicoletatum <nicoletatum@outlook.com>
2021-07-06 15:32:21 -0500

    swapped whitelist cors

--- plantapp/settings.py
+++ plantapp/settings.py
@@ -75,7 +75,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://my-plant-care.herokuapp.com'
+    'https://plant--care.herokuapp.com'
 )
 
 MIDDLEWARE = [
